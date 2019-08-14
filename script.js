a3422d59cb435a6b3b4e68710d6f336dd3f3e227
Dustin Newbold <dustinnewbold@dustins-mbp.lan>
2019-08-14 16:26:23 -0400

    Updated to support older browsers

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => {
+var buttons = document.querySelectorAll('button');
+buttons.forEach(function(button) {
     button.addEventListener('click', verbRequest);
 });
 
 function verbRequest() {
-    const requestURL = '/test.json';
-    const button = this;
-    const httpVerb = button.dataset.verb;
-    const requestType = button.parentElement.dataset.type;
+    var requestURL = '/test.json';
+    var button = this;
+    var httpVerb = button.dataset.verb;
+    var requestType = button.parentElement.dataset.type;
 
     if ( ! httpVerb ) console.error('Button has no verb, weirdo');
     if ( ! requestType ) console.error('Cannot figure out request type');
 
     if ( requestType === 'fetch' ) {
         fetch('/test.json', { method: httpVerb })
-            .then(request => {
-                return request.json()
+            .then(function(request) {
+                return request.json();
             })
-            .then(data => {
+            .then(function(data) {
                 console.log(data);
             });
     } else {
